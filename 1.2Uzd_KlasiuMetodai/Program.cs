1b04a03cbd66ccea2e62c1ddaee09bbb6193ed85
Laura <Laura@DESKTOP-BESSB6E>
2023-10-05 21:11:55 +0300

    Add project files.

---
+++ 1.2Uzd_KlasiuMetodai/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _1._2Uzd_KlasiuMetodai
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rectangle = new Rectangle();
+            rectangle.Heigth = Convert.ToDouble(Console.ReadLine());
+            rectangle.Width = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(rectangle.Perimeter());
+            Console.WriteLine(rectangle.Area());    
+        }
+    }
+}
---
+++ 1.2Uzd_KlasiuMetodai/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2Uzd_KlasiuMetodai
+{
+    public class Rectangle
+    {
+        public double Heigth { get; set; }
+        public double Width { get; set; }
+
+
+
+        public double Perimeter() 
+        {
+            double sum = Heigth + Width;
+            double per = sum * 2;
+            return per;
+
+        }
+        public double Area()
+        {
+            double area = Heigth * Width;
+            return area;
+        }
+    }
+}
